2d7b5ffe4ee215f2125ae7411f8a04abe435bf0c
LigualLink <lixiangban1023@qq.com>
2024-02-29 14:53:09 +0800

    day1

---
+++ Day1: function/day1_function.py
@@ -0,0 +1,85 @@
+"""
+这个文件提供了学习Python函数时用到的代码
+"""
+import sys
+import os
+import time
+
+# 为了使父目录的模块被此脚本导入, 在脚本开始之时添加如下代码.
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from python_data import nums
+    
+"""
+装饰器
+"""
+def d_calculate_execution_time(func):
+    """这是一个装饰器，将打印所装饰的函数func的执行时间
+
+    Args:
+        func (_type_): 通常是一个数组
+    """    
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"函数{func.__name__}的执行时间为：{execution_time}秒")
+        return result
+    return wrapper
+
+def merge(nums, start, end):
+    """这是归并排序的归并操作
+
+    Args:
+        nums (List[int]): _description_
+        start (int): _description_
+        end (int): _description_
+    """    
+    mid = start + (end - start) // 2
+    L = nums[start: mid + 1]
+    R = nums[mid + 1: end + 1]
+    
+    i, j, k = 0, 0, start
+    while i < len(L) and j < len(R):
+        if L[i] > R[j]:
+            nums[k] = R[j]
+            j += 1
+        else:
+            nums[k] = L[i]
+            i += 1
+        k += 1
+    while i < len(L):
+        nums[k] = L[i]
+        i += 1
+        k += 1
+    while j < len(R):
+        nums[k] = R[j]
+        j += 1
+        k += 1
+        
+def merge_sort_recursion(nums, start, end):
+    """这是归并排序的递归函数
+
+    Args:
+        nums (_type_): _description_
+        start (_type_): _description_
+        end (_type_): _description_
+    """    
+    if end > start:
+        mid = start + (end - start) // 2
+        merge_sort_recursion(nums, start, mid)
+        merge_sort_recursion(nums, mid + 1, end)
+        merge(nums, start, end)
+        
+@d_calculate_execution_time
+def merge_sort(nums):
+    """这是归并排序的入口
+
+    Args:
+        nums (_type_): _description_
+    """   
+    merge_sort_recursion(nums, 0, len(nums) - 1)
+    return nums 
+         
+merge_sort(nums)
